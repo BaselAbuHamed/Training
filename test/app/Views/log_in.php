9a75482dd035990acf6d22396a02a5e1a5d6fe16
Basel Z. Abu Hamed <basel76ziyad24@gmail.com>
2023-08-29 08:33:49 +0300

    commit

--- test/app/Views/log_in.php
+++ test/app/Views/log_in.php
@@ -1,9 +1,9 @@
 <?php
-//session_start();
-//if (isset($_SESSION['email'])) {
-//    header("Location: ../../public/index.php");
-//    exit();
-//}
+session_start();
+if (isset($_SESSION['email'])) {
+    header("Location: ../../public/index.php");
+    exit();
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
