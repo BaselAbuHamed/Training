e078341d27f477dcca314fbf81ece5baa97e54c2
Basel Z. Abu Hamed <basel76ziyad24@gmail.com>
2023-09-07 08:41:48 +0300

    commit

--- QuizMakerMVC/app/classes/error_log.php
+++ QuizMakerMVC/app/classes/error_log.php
@@ -1,12 +1,17 @@
 <?php
-
+/*The logError function you've provided is indeed useful for logging error messages
+ and debugging PHP code by printing values to a file. You can use this function
+ to log any information you want during debugging to help you trace issues
+in your PHP application.*/
 function logError($message) {
 
     $logFile = '../../src/error_log.txt';
     $timestamp = date('Y-m-d H:i:s');
-    $errorLogEntry = "[$timestamp] ==> $message\n";
-    // Append the error message to the log file
 
+    // Create a log entry by combining the timestamp and the provided error message.
+    $errorLogEntry = "[$timestamp] ==> $message\n";
+
+    // Append the error message to the log file
+    // (FILE_APPEND flag ensures appending rather than overwriting).
     file_put_contents($logFile, $errorLogEntry, FILE_APPEND);
 }
-
